e9bfebe0b99b336da86aa31803d1251780eeb077
sarun <sarunj@stud.ntnu.no>
2023-05-30 00:23:37 +0200

    #4 added more text to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,19 @@ import React from 'react';
 const Home = () => {
   return (
     <div className='content-container'>
-      <div className='flex flex-row items-center justify-left w-full h-full p-4 bg-gray-100 rounded-lg shadow-lg'>  
-        <h1 className='font-nameHeaderFont text-3xl text-left'>Hi, Im Bernard Nicolas Brillanti. </h1>
+      <div className='items-center justify-left w-full h-full p-4 my-6 bg-gray-100 rounded-lg shadow-lg'>  
+        <h1 className='font-mono font-bold text-6xl text-left'>Hello, Im Bernard</h1>
+        <h2 className='font-mono font-bold text-2xl text-left'>A French sculptor blending nature and society to provoke thought and invite interaction through my art.</h2>
+        
+        <p className='font-mono font-bold text-xl text-left'>I create sculptures and art pieces for private and public spaces.</p>
+
       </div>
-       
+
+      <div className='items-center p-4 h-full w-full border-2 border-red-500 bg-gray-300'>
+        <h1>3D modell or Image</h1>
+        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi </p>
+      </div>
         
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,19 +15,19 @@ const Navbar = () => {
         </div>
 
       <ul className='hidden md:flex'>
-        <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>
+        <li className='px-4 cursor-pointer capitalize font-semibold text-gray-500 hover:scale-110 duration-200'>
           <Link to='/'>Home</Link>
         </li>
-        <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>
+        <li className='px-4 cursor-pointer capitalize font-semibold text-gray-500 hover:scale-110 duration-200'>
           <Link to='/sculpture-gallery'>Sculpture Gallery</Link>
         </li>
-        <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>
+        <li className='px-4 cursor-pointer capitalize font-semibold text-gray-500 hover:scale-110 duration-200'>
           <Link to='/my-work'>My Work</Link>
         </li>
-        <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>
+        <li className='px-4 cursor-pointer capitalize font-semibold text-gray-500 hover:scale-110 duration-200'>
           <Link to='/about-me'>About Me</Link>
         </li>
-        <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>
+        <li className='px-4 cursor-pointer capitalize font-semibold text-gray-500 hover:scale-110 duration-200'>
           <Link to='/contact'>Contact</Link>
         </li>
       </ul>
