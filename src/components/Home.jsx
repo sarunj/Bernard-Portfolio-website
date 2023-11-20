a25171d3ebca42b9d79619a0c9ee3115eabca72d
sarun <sarunj@stud.ntnu.no>
2023-11-20 21:27:08 +0100

    added 3d model to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-
+import Model3D from './Model3D';
+// import img_3D from '/redheart.png';
 
 const Home = () => {
   return (
     <div className='content-container'>
-      <div className='items-center justify-left w-full h-full p-4 my-6 bg-gray-100 rounded-lg shadow-lg'>  
+      <div className='items-center justify-left w-full h-full py-4 pl-5 my-6 bg-gray-100 rounded-lg '>  
         <h1 className='font-mono font-bold text-6xl text-left'>Hello, Im Bernard</h1>
         <h2 className='font-mono font-bold text-2xl text-left'>A French sculptor blending nature and society to provoke thought and invite interaction through my art.</h2>
         
-        <p className='font-mono font-bold text-xl text-left'>I create sculptures and art pieces for private and public spaces.</p>
+        <p className='font-mono font-bold text-xl text-left'>I create sculptures and art pieces for private and' public spaces.</p>
 
       </div>
 
-      <div className='items-center p-4 h-full w-full border-2 border-red-500 bg-gray-300'>
-        <h1>3D modell or Image</h1>
-        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
-          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi </p>
-      </div>
-        
+      {/* <div className='items-center justify-items-end p-4 h-[600px] w-[400px] border-2 border-red-500 bg-gray-300'>
+        <img src='images/redheart.png' alt='3D' className='rounded-2xl bg-center bg-cover duration-500'/>
+      </div> */}
+      <Model3D /> 
+
     </div>
   )
 }
--- src/components/Model3D.jsx
+++ src/components/Model3D.jsx
@@ -1,20 +1,40 @@
 import React from 'react'
 import { Canvas } from "@react-three/fiber";
 import { PresentationControls, Stage } from "@react-three/drei";
+import RedHeart from '../models/RedHeart';
 
 function Model3D() {
   return (
-    <div>
+    <div className='h-[600px] w-[400px] border-2 border-gray-500 bg-gray-300'>
         <h1>3D Model</h1>
+        {/* https://codesandbox.io/s/qyz5r?file=/src/App.js:431-507 */}
         <Canvas>
-            <Stage controls={PresentationControls} intensity={1} contactShadowOpacity={1} shadows adjustCamera={false}>
-                <mesh receiveShadow castShadow>
-                    <boxBufferGeometry args={[1, 1, 1]} />
-                    <meshStandardMaterial color="hotpink" />
-                </mesh>
-            </Stage>
-        </Canvas>
-        
+          {/* TODO: make react-suspence and loader component */}
+          <PresentationControls
+              // config={{ mass: 100, tension: 500 }}
+              snap={{ mass: 4, tension: 100 }}
+              rotation={[0, 0, 0]}
+              speed={1.5}
+              global={true}
+              zoom={1.5}
+              polar={[-0.3, Math.PI / 4]}
+            >
+              <Stage>
+                  <RedHeart />
+              </Stage>
+            </PresentationControls>
+        </Canvas>    
+
+        {/* <PresentationControls
+              speed={1.5}
+              global
+              zoom={1.5}
+              polar={[-0.3, Math.PI / 4]}
+            >
+              <Stage environment={null}>
+                <Model scale={0.01} />
+              </Stage>
+            </PresentationControls> */}
     </div>
   )
 }
